49ecf62d500302d819aeabf33e46ecda9917fa01
Ace <ace@mdev.digital>
2018-09-25 16:24:33 -0400

    Created Smarter Living Parent for dev

--- webpack.common.js
+++ webpack.common.js
@@ -191,6 +191,10 @@ module.exports = {
       template: './src/templates/typography-test.pug',
       filename: 'test/typography.html'
     }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/smarter-parent.pug',
+      filename: 'smarter-living.html'
+    }),
     new CopyWebpackPlugin([
       { from: 'src/js', to: 'js', force: true }
     ])
