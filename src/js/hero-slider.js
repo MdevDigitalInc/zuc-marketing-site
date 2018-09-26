dfd385d2fdd5d007800bd6d6368327853f115103
Lucas Moreira <lucas@moreiradevelopment.io>
2018-09-26 11:24:54 -0400

    Added transitions and function to normalize container

--- src/js/hero-slider.js
+++ src/js/hero-slider.js
@@ -1,9 +1,12 @@
 // [ Hero Slider Controller ]
-//
 (function(){
   var desiredSlide = null;
+  var resizeTimer;
   // Make sure we are live and ready..
   $(document).ready(function() {
+    // Normalize Container
+    normalizeSlider();
+
     $('[data-move-slide]').click(function(){
       // Only trigger if user clicks an inactive icon
       if (!$(this).hasClass('--active')) {
@@ -18,4 +21,23 @@
       }
     });
   });
+  // Resize Events (debounced)
+  // -----------------------------
+  $(window).on('resize', function(e) {
+    // Clear Timer
+    clearTimeout(resizeTimer);
+    // Fire function after 250ms
+    resizeTimer = setTimeout(function() {
+      // Normalize Dropdowns Again...
+      normalizeSlider();
+    },250);
+  });
+
+  // Fix height of container based on contents
+  function normalizeSlider() {
+    containerHeight = $('[data-slide]')[0].getBoundingClientRect().height;
+    $('[data-slide-container]').css({
+      'height': containerHeight + 'px'
+    });
+  };
 })();
