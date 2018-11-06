2559f00a1d174476f3bb33a5becd50ee2cc78bd1
Lucas Moreira <lucas@moreiradevelopment.io>
2018-11-06 11:11:23 -0500

    HOTFIX | NPM & Webpack Dependencies - Critical (#184)
    * Fixed webpack for newer macs
    
    * Further modifications to eliminate security vulnerabilities

--- webpack.common.js
+++ webpack.common.js
@@ -66,7 +66,7 @@ module.exports = {
           exclude: /(node_modules)/,
           use: [{
             loader: "babel-loader",
-            options: { presets: ['es2015'] }
+            options: { presets: ['babel-preset-es2015'] }
           }]
       },
       // CSS Processing
