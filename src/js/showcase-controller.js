5db35b71ee65081dcdb60042e1f0c6b8cce4499b
GcouttsMDev <graham@mdev.digital>
2018-11-08 09:54:14 -0500

    Added js controller to change the showcase image based on which of the smaller showecase images you click on

---
+++ src/js/showcase-controller.js
@@ -0,0 +1,17 @@
+
+
+function changeMainImage(input) {
+    
+    $('.--active-image').removeClass('--active-image');
+    // Select the correct review based on the math above..
+    desiredTarget = $('[showcase-small]')[input];
+    
+    $(desiredTarget).addClass('--active-image');
+
+    picture = $('[showcase-image]')[input]
+
+    bigPicture = $('[showcase-big]')[0]
+
+    bigPicture.src = picture.src;
+    bigPicture.alt = picture.alt;
+  }
