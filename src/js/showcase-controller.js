d2dce69aded38c32268d3c3cbeec1147f902b370
GcouttsMDev <graham@mdev.digital>
2018-11-12 10:04:14 -0500

    updated the classes to match standard.

--- src/js/showcase-controller.js
+++ src/js/showcase-controller.js
@@ -2,13 +2,13 @@ function changeMainImage(input) {
     
     $('.--active-image').removeClass('--active-image');
     // Select the correct review based on the math above..
-    desiredTarget = $('[showcase-small]')[input];
+    desiredTarget = $('[data-showcase-small]')[input];
     
     $(desiredTarget).addClass('--active-image');
 
-    picture = $('[showcase-image]')[input]
+    picture = $('[data-showcase-image]')[input]
 
-    bigPicture = $('[showcase-big]')[0]
+    bigPicture = $('[data-showcase-big]')[0]
 
     bigPicture.src = picture.src;
     bigPicture.alt = picture.alt;
